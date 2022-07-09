d1dc4416bc949606ea9230c3ccef1115638f8a4e
Tenorun <ngdtv123@gmail.com>
2022-07-09 17:43:36 +0900

    asdf

--- asdf.py
+++ asdf.py
@@ -1,5 +1,9 @@
-#include <stdio.h>
-int main() {
-	puts("SMAAAASH!");
-	return 0;
-}
+K = int(input())
+account_book = None
+
+for i in range(K):
+	Input = int(input())
+    if Input == 0:
+        account_book.pop()
+    else:
+        account_book.append(Input)
